ab3964a9f8b799a48af2ef584e6b8c2d3ef6c0a3
Raneen Mahmoud <raneenm260@gmail.com>
2023-04-02 15:07:08 +0200

    edit migration
     edit doctor migration

--- database/migrations/2023_03_28_215253_create_doctors_table.php
+++ database/migrations/2023_03_28_215253_create_doctors_table.php
@@ -13,8 +13,9 @@ return new class extends Migration
     {
         Schema::create('doctors', function (Blueprint $table) {
             $table->id();
-            $table->boolean('is_ban');
-            $table->date('created_at')->format('d/m/Y');
+            $table->boolean('is_ban')->default(false);
+            // $table->date('created_at')->format('d/m/Y');
+            $table->timestamps();
         });
     }
 
